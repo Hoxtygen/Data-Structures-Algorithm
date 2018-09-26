f207a566bb4019d8561cba8a04044cbeb1c6c4d1
hoxtygen <hoxtygen@live.com>
2018-09-26 23:10:06 +0100

    refactor code and removed extra variable
    Code essence added and extra variable deleted

--- Algorithm/Sorting-Algorithm/Recursion/arrayAdd.js
+++ Algorithm/Sorting-Algorithm/Recursion/arrayAdd.js
@@ -1,11 +1,11 @@
+//Add together items in a list
+
 function add(arr) {
-    let total = 0;
     if (arr.length === 0) {
         return 0;
     }else {
-      total = arr[0] + add(arr.slice(1));
+       return  arr[0] + add(arr.slice(1));
     }
-    return total;
 }
 console.log(add([1, 2, 3, 4, 5]));
 
