e4b334337e22c204f90abc8643d02f7b791c9052
hoxtygen <hoxtygen@live.com>
2018-09-26 21:26:38 +0100

    Array.slice usage
    This little added code snippet shows usage of array.slice(), to properly
    understand how slice works in the main code snipppet.

--- Algorithm/Sorting-Algorithm/Recursion/arrayAdd.js
+++ Algorithm/Sorting-Algorithm/Recursion/arrayAdd.js
@@ -8,3 +8,12 @@ function add(arr) {
     return total;
 }
 console.log(add([1, 2, 3, 4, 5]));
+
+//Understanding Array.slice(), makes no change to the original array unlike Array.splice that cuts out from the original
+//array.
+let myArr = [1, 2, 3, 4, 5, 6, 7];
+let newArr = myArr.slice(1, 5)
+console.log(newArr);
+console.log(myArr);
+
+
