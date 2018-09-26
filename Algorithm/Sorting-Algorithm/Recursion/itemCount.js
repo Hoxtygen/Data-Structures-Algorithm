74ab2fae9085b57947b531dbd00e4bd0194e631b
hoxtygen <hoxtygen@live.com>
2018-09-26 22:04:56 +0100

    Working code snippets of code works to count number of items in an array

--- Algorithm/Sorting-Algorithm/Recursion/itemCount.js
+++ Algorithm/Sorting-Algorithm/Recursion/itemCount.js
@@ -1,7 +1,9 @@
 function itemCount(items) {
-    if (items.lenth === 0) {
+    if (items.length === 0) {
         return 0;
     } else {
-        
+        return 1 + itemCount(items.slice(1))
     }
 }
+
+console.log(itemCount([1, 2, 3, 4, 5, 9, 11, 34, 87,9]));
